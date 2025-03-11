e55d998d67032a0bcc5778ce9969e822bf615a4b
Filxo2n <109392803+Filxo2n@users.noreply.github.com>
2025-03-11 21:45:53 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker instalado.");
+    event.waitUntil(
+        caches.open("cache-v1").then((cache) => {
+            return cache.addAll(["index.html", "styles.css", "script.js", "manifest.json"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
